f8b9a353b0259847e4e136b0f50a97f718ea5fab
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-02 16:07:18 +0530

    Create HackerRank Tree: Inorder Traversal.java

---
+++ day2/HackerRank Tree: Inorder Traversal.java
@@ -0,0 +1,10 @@
+ public static void inOrder(Node root) {
+        if(root==null)
+        {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+
+    }
