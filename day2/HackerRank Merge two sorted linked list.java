8920615b1e0490744a15a2bc3ea31968a7412343
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-02 13:38:08 +0530

    Update HackerRank Merge two sorted linked list.java

--- day2/HackerRank Merge two sorted linked list.java
+++ day2/HackerRank Merge two sorted linked list.java
@@ -1,51 +1,47 @@
-static SinglyLinkedListNode head3=null;
-      static SinglyLinkedListNode tail3=null;
-     static void insert(int val)
+ static SinglyLinkedListNode insert(SinglyLinkedListNode head3,int val)
      { 
-         if(head3==null)
-         {
-             head3=new SinglyLinkedListNode(val);
-             tail3=head3;
-         }
-         else
-         {
-             SinglyLinkedListNode node=new SinglyLinkedListNode(val);
-             tail3.next=node;
-             tail3=node;
-         }
+        
+        SinglyLinkedListNode node=new SinglyLinkedListNode(val);
+        if(head3==null)
+        {
+            head3=node;
+            return head3;
+        } 
+        SinglyLinkedListNode current=head3;
+        while(current.next!=null)
+        {
+            current=current.next;
+        }
+        current.next=node;
+        return head3;
+        
      }
     static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-        
+        SinglyLinkedListNode head3=null;        
         SinglyLinkedListNode current1 =head1;
         SinglyLinkedListNode current2 =head2;
         while(current1!=null && current2!=null)
         {
-            if(current1.data==current2.data)
+            
+            if(current1.data<current2.data)
             {
-                insert(current1.data);
-                insert(current2.data);
-                current1=current1.next;
-                current2=current2.next;
-            }
-            else if(current1.data<current2.data)
-            {
-                insert(current1.data);
+               head3= insert(head3,current1.data);
                 current1=current1.next;  
             }
             else
             {
-                insert(current2.data);
+                head3=insert(head3,current2.data);
                 current2=current2.next; 
             }
         }
         while(current1!=null)
         {
-            insert(current1.data);
+            head3= insert(head3,current1.data);
             current1=current1.next;
         }
          while(current2!=null)
         {
-            insert(current2.data);
+           head3= insert(head3,current2.data);
             current2=current2.next;
         }
         return head3;
