d142aa059e20ab0ee18dcd6f1f1bcf23a5434dc7
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-02 16:04:27 +0530

    Create HackerRank Tree: Postorder Traversal.java

---
+++ day2/HackerRank Tree: Postorder Traversal.java
@@ -0,0 +1,11 @@
+ public static void postOrder(Node root) {
+        if(root==null)
+        {
+            return;
+        }
+        
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data +" ");
+
+    }
