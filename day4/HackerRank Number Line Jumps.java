6d819b3a8802567d6306b01607fc4cf95517acc1
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-04 18:27:39 +0530

    Create HackerRank Number Line Jumps.java

---
+++ day4/HackerRank Number Line Jumps.java
@@ -0,0 +1,26 @@
+ public static String kangaroo(int x1, int v1, int x2, int v2) {
+    // Write your code here
+    if((x1<x2 && v1<v2)||(x2<x1 && v2<v1))
+        return "NO";
+    if(x1<x2)
+    {
+        while(x1<x2)
+        {
+            x1=x1+v1;
+            x2=x2+v2;
+            if(x1==x2)
+                return "YES";
+        }
+    }
+    if(x1>x2)
+    {
+        while(x1>x2)
+        {
+            x1=x1+v1;
+            x2=x2+v2;
+            if(x1==x2)
+                return "YES";
+        }
+    }
+    return "NO";
+    }
