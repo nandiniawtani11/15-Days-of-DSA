8bd79e9f2c1d3e6170a5084d3e862342f80634b3
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-05 20:14:04 +0530

    Create Fibonacci using Recursion.java

---
+++ day5/Fibonacci using Recursion.java
@@ -0,0 +1,20 @@
+package dsa;
+
+public class fibonaci {
+	public static int fib(int n)
+	{
+		if(n==0) 
+			return 0;
+		if(n==1) 
+			return 1;
+		return fib(n-1)+fib(n-2);
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		int k=fib(10);
+		System.out.print(k);
+	}
+
+}
