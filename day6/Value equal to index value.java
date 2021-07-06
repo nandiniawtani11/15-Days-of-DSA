83ff0e004faa78c30a5c25f58b1c07c0ec86e2ea
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-06 14:02:14 +0530

    Create Value equal to index value.java

---
+++ day6/Value equal to index value.java
@@ -0,0 +1,14 @@
+class Solution {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==i+1)
+            {
+                list.add(arr[i]);
+            }
+        }
+        return list;
+    }
+}
