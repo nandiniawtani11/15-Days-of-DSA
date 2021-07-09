d6609a36c0ba25d3a5eee6f78503aef6c51f7ce3
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-07-09 07:50:23 +0530

    Create LeetCode  Perfect Number.java

---
+++ day8/LeetCode  Perfect Number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<=num/2;i++)
+        {
+            if(num%i==0)
+                sum=sum+i;
+            if(sum>num)
+                return false;     
+        }
+        if (sum==num)
+            return true;
+        else return false;
+        
+    }
+}
